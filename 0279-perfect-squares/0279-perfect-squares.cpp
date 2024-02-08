174320782c257ef9442fb63659e3d94f7c1645af
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-02-08 13:51:14 +0530

    Time: 1192 ms (5.01%), Space: 40.1 MB (9.65%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int helper(vector<int>& gg,int i,int k,vector<int>& ggg)
+    {
+        if(i>=gg.size()||k<0)
+            return INT_MAX-1;
+        if(k==0)
+            return 0;
+        if(ggg[k]!=-1)
+        return ggg[k];
+        
+        return ggg[k]=min({1+helper(gg,i,k-gg[i],ggg),1+helper(gg,i+1,k-gg[i],ggg),helper(gg,i+1,k,ggg)});
+    }
+    int numSquares(int k) {
+        
+        vector<int>gg;
+        vector<int>ggg (10001,-1);
+        for(int i=1;i*i<=k;i++)
+        {
+          gg.push_back(i*i);  
+        }
+         
+        return helper(gg,0,k,ggg);
+    }
+};
