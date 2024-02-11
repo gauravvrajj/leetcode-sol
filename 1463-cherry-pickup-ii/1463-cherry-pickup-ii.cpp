ccef4b212e79a1c0069f24683d0fc9d908a67a54
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-02-11 20:55:52 +0530

    Time: 101 ms (43.71%), Space: 150.9 MB (5.03%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    
+    int gg(int row,int col1,int col2,vector<vector<int>>&grid,vector<vector<vector<int>>>&dp)
+    {
+        if(row==grid.size()||col1<0||col1>=grid[0].size()||col2<0||col2>=grid[0].size())
+            return 0;
+        if(dp[row][col1][col2]!=-1)
+            return dp[row][col1][col2];
+        int curr=grid[row][col1]+grid[row][col2];
+        if(col1==col2)
+            curr-=grid[row][col1];
+        int ans=INT_MIN;
+        ans=max(ans,curr+gg(row+1,col1+1,col2,grid,dp));
+        ans=max(ans,curr+gg(row+1,col1,col2+1,grid,dp));
+        ans=max(ans,curr+gg(row+1,col1+1,col2+1,grid,dp));
+        
+        ans=max(ans,curr+gg(row+1,col1-1,col2,grid,dp));
+        ans=max(ans,curr+gg(row+1,col1,col2-1,grid,dp));
+        ans=max(ans,curr+gg(row+1,col1-1,col2-1,grid,dp));
+        
+        ans=max(ans,curr+gg(row+1,col1,col2,grid,dp));
+        ans=max(ans,curr+gg(row+1,col1+1,col2-1,grid,dp));
+        ans=max(ans,curr+gg(row+1,col1-1,col2+1,grid,dp));
+        
+     return dp[row][col1][col2]=ans;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        
+        vector<vector<vector<int>>>abc(71,vector<vector<int>>(71,vector<int>(71,-1)));
+        return gg(0,0,grid[0].size()-1,grid,abc);
+    }
+};
