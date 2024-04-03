a722e8ac6fa2deb5bddcd4bef455a9a912814066
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-04-03 13:19:32 +0530

    Time: 198 ms (89.21%), Space: 9.6 MB (89.06%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<char>>&board,int i,int j,int k,string& word,vector<vector<bool>>& visited)
+    bool dfs(vector<vector<char>>&board,int i,int j,int k,string& word)
     {
         if(k==word.size())
             return 1;
         if(i>=board.size()||i<0||j>=board[0].size()||j<0)
             return 0;
-        if(board[i][j]!=word[k]||visited[i][j]==1)
+        if(board[i][j]!=word[k]||board[i][j]=='#')
             return 0;
-        visited[i][j]=1;
-        bool b= dfs(board,i+1,j,k+1,word,visited)||
-        dfs(board,i,j+1,k+1,word,visited)||
-        dfs(board,i-1,j,k+1,word,visited)||
-        dfs(board,i,j-1,k+1,word,visited);
-        if(b==0)
-            visited[i][j]=0;
+        char ch=board[i][j];
+        board[i][j]='#';
+        bool b= dfs(board,i+1,j,k+1,word)||
+        dfs(board,i,j+1,k+1,word)||
+        dfs(board,i-1,j,k+1,word)||
+        dfs(board,i,j-1,k+1,word);
+        board[i][j]=ch;
         return b;
     }
     bool exist(vector<vector<char>>& board, string word) {
@@ -24,9 +24,9 @@ public:
         {
             for(int j=0;j<board[i].size();j++)
             {
-                vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),0));
+               
                 if(board[i][j]==word[0])
-                    if(dfs(board,i,j,0,word,visited))
+                    if(dfs(board,i,j,0,word))
                         return 1;
             }
         }
