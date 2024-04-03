a792b03a66288136f4c7b380ee39441d63669068
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-04-03 13:16:12 +0530

    Time: 522 ms (60.05%), Space: 11.3 MB (61.35%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<char>>&board, string& word,vector<vector<bool>>&visited,int i,int j,int k)
+    bool dfs(vector<vector<char>>&board,int i,int j,int k,string& word,vector<vector<bool>>& visited)
     {
         if(k==word.size())
-        return 1;
-        if(i<0||j>=board[0].size()||i>=board.size()||j<0||visited[i][j])
-        return 0;
-        if(board[i][j]!=word[k])
-        return 0;
+            return 1;
+        if(i>=board.size()||i<0||j>=board[0].size()||j<0)
+            return 0;
+        if(board[i][j]!=word[k]||visited[i][j]==1)
+            return 0;
         visited[i][j]=1;
-        bool ans= dfs(board,word,visited,i+1,j,k+1)||
-                  dfs(board,word,visited,i,j+1,k+1)||
-                  dfs(board,word,visited,i-1,j,k+1)||
-                  dfs(board,word,visited,i,j-1,k+1);
-        visited[i][j]=0;
-        return ans;
+        bool b= dfs(board,i+1,j,k+1,word,visited)||
+        dfs(board,i,j+1,k+1,word,visited)||
+        dfs(board,i-1,j,k+1,word,visited)||
+        dfs(board,i,j-1,k+1,word,visited);
+        if(b==0)
+            visited[i][j]=0;
+        return b;
     }
-    
     bool exist(vector<vector<char>>& board, string word) {
         
-       
         for(int i=0;i<board.size();i++)
-            for(int j=0;j<board[0].size();j++)
-            {   bool b=0;
+        {
+            for(int j=0;j<board[i].size();j++)
+            {
+                vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),0));
                 if(board[i][j]==word[0])
-                {
-                    vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),0));
-                    b=dfs(board,word,visited,i,j,0);
-                }
-                if(b)
-                    return b;
+                    if(dfs(board,i,j,0,word,visited))
+                        return 1;
             }
+        }
         
         return 0;
         
