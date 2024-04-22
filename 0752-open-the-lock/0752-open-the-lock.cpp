44ab29deb78083d576d0eaeb3cc0aecdc029045d
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-04-22 22:09:28 +0530

    Time: 608 ms (7.99%), Space: 146.1 MB (6.12%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    vector<string> children(string& a)
+    {
+        vector<string>ans;
+        for(int i=0;i<4;i++)
+        {  string gg=a,pp=a;
+            gg[i]=(gg[i]-'0'+1)%10+'0';
+            ans.push_back(gg);
+            pp[i]=(pp[i]-'0'-1+10)%10+'0';
+            ans.push_back(pp);
+        }
+        for(auto i:ans)
+            cout<<i<<" ";
+        return ans;
+    }
+    int openLock(vector<string>& deadends, string target) {
+        
+        unordered_set<string>visited(deadends.begin(),deadends.end());
+        if(visited.count("0000"))return -1;
+        queue<pair<string,int>>gg;
+        gg.push({"0000",0});
+        
+        while(gg.size())
+        {
+            pair<string,int> m=gg.front();
+            gg.pop();
+            if(m.first==target)
+                return m.second;
+            for(auto child:children(m.first))
+            {
+                if(!visited.count(child))
+                {
+                    visited.insert(child);
+                    gg.push({child,m.second+1});
+                }
+            }
+        }
+        
+        return -1;
+        
+    }
+};
