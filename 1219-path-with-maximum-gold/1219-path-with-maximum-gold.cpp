71a8deea38e1e589d9d6d10650b3c8dfb25b0288
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-05-14 13:45:59 +0530

    Time: 122 ms (66.71%), Space: 11.9 MB (11.32%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ans=INT_MIN;
+    void dfs(vector<vector<int>>&grid,int i,int j,int sum)
+    {
+        if(i<0||j>=grid[0].size()||i>=grid.size()||j<0||grid[i][j]==0)
+        {
+            ans=max(ans,sum);
+            return;
+        }
+        int a=grid[i][j];
+        grid[i][j]=0;
+        dfs(grid,i+1,j,sum+a);
+        dfs(grid,i,j+1,sum+a);
+        dfs(grid,i,j-1,sum+a);
+        dfs(grid,i-1,j,sum+a);
+        grid[i][j]=a;
+        
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        
+        for(int i=0;i<grid.size();i++)
+            for(int j=0;j<grid[0].size();j++)
+            {
+                vector<vector<int>>gg=grid;
+                if(grid[i][j]!=0)dfs(gg,i,j,0);
+            }
+        return ans==INT_MIN?0:ans;
+    }
+};
