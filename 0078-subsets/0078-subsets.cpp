e6346f56d0b68c5b804e3012e8f7d42b8f46b965
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-05-21 12:16:11 +0530

    Time: 4 ms (39.89%), Space: 8.3 MB (81.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    
-    vector<vector<int>> ans;
-    
-    void unique(int i,vector<int>&nums,vector<int>&gg)
-    {
-        ans.push_back(gg);
-        for(int l=i;l<nums.size();l++)
-        {
-            gg.push_back(nums[l]);
-      
-            unique(l+1,nums,gg);
-            
-            gg.pop_back();
-        
-        }
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>gg;
-        unique(0,nums,gg);
-        return ans;
+        vector<vector<int>> subs = {{}};
+        for (int num : nums) {
+            int n = subs.size();
+            for (int i = 0; i < n; i++) {
         
+                subs.push_back(subs[i]); 
+                subs.back().push_back(num);
+            }
+        }
+        return subs;
     }
 };
