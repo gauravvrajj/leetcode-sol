2efc1d28f99217e52b68a134124a332d34526694
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-06-03 14:14:16 +0530

    Time: 16 ms (74.78%), Space: 11.9 MB (83.42%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        int maxx=0,prev;
-        for(int i=0;i<s.size();)
+        
+        int j=0,k=0;
+        
+        while(j<s.size()&&k<t.size())
         {
-            if(s[i]==t[0])
-            {  int j=1,k=i+1,count=1;
-               bool b=0;
-                while(k<s.size()&&j<t.size())
-                {
-                    if(s[k]==t[0]&&b==0)
-                    {
-                      i=k;
-                      b=1;
-                    }
-                    if(s[k]==t[j])
-                    {
-                    count++;
-                    k++;
-                    j++;
-                    }
-                    else 
-                    k++;
-                }
-              maxx=max(count,maxx);
-             if(b==0)break;
-            }
-            else
-            {
-                i++;
-            }
-            if(maxx==t.size())return 0;
+            if(s[j]==t[k])
+                k++;
+            j++;
         }
         
-        return t.size()-maxx;
+        return t.size()-k; 
+        
     }
 };
