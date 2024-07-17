17a80c713851f6a4f82d10429305307513c0e131
gauravvrajj <70380003+gauravvrajj@users.noreply.github.com>
2024-07-17 13:28:14 +0530

    Time: 16 ms (56.71%), Space: 27.7 MB (16.35%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -12,46 +12,39 @@
 class Solution {
 public:
     vector<TreeNode*> ans;
-    unordered_set<TreeNode*> toDeleteNodes;
-    
-    void helper(TreeNode* root, unordered_set<int>& toDelete, TreeNode* parent, bool isLeftChild) {
-        if (!root) return;
-
-        // Recursively process the left and right children
-        helper(root->left, toDelete, root, true);
-        helper(root->right, toDelete, root, false);
-
-        // If current node needs to be deleted
-        if (toDelete.count(root->val)) {
-            if (root->left) ans.push_back(root->left);
-            if (root->right) ans.push_back(root->right);
-
-            // If the parent exists, disconnect the current node from the parent
-            if (parent) {
-                if (isLeftChild) parent->left = nullptr;
-                else parent->right = nullptr;
-            }
-
-            // Mark the node for deletion
-            toDeleteNodes.insert(root);
+    unordered_set<TreeNode*> ff;
+    TreeNode* helper(TreeNode* root,  unordered_set<int>& gg){
+        
+        if(!root)
+            return NULL;
+        
+        root->left = helper(root->left,gg);
+        root->right = helper(root->right,gg);
+        
+        if(gg.count(root->val))
+        {   
+            if(root->left)
+                ans.push_back(root->left);
+            if(root->right)
+                ans.push_back(root->right);
+            delete root;
+            return NULL;
         }
+        
+        return root;
     }
-
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& toDelete) {
-        unordered_set<int> toDeleteSet(toDelete.begin(), toDelete.end());
-
-        helper(root, toDeleteSet, nullptr, false);
-
-        // If the root node is not deleted, add it to the result forest
-        if (!toDeleteSet.count(root->val)) {
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        
+        unordered_set<int> gg;
+        for(auto i:to_delete)
+        {
+            gg.insert(i);
+        }
+        root = helper(root,gg);
+        if(root)
+        {
             ans.push_back(root);
         }
-
-        // Delete the marked nodes
-        for (TreeNode* node : toDeleteNodes) {
-            delete node;
-        }
-
         return ans;
     }
 };
